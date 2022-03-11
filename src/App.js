5ff4b284c67373b2973b3c67ea630d9f13402d89
CodexJE <nasriferesd@gmail.com>
2022-03-11 11:48:45 +0100

    new branch

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          New feature bitches
         </a>
       </header>
     </div>
